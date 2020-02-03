d1757a79e89e88926c33117cef4c5f87ed05af3e
Marek Klatt <531devv@gmail.com>
2020-02-03 17:10:35 +0100

    Adding HTML, CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import Hello from './Hello';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Hello />
+    <div className="App">
+      <header>
+        <h1>TODO App</h1>
+      </header>
+      <form id="todo-form">
+        <input type="text" placeholder="Text.." />
+      </form>
+    </div>
     );
   }
 }
