620336a2bb520c80dded036f242cccd94be6756e
Marek Klatt <531devv@gmail.com>
2020-02-03 17:37:28 +0100

    Adding functions to App Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text != "") {
+      const newItems = [...this.state.items, newItems];
+      this.setState({
+        items: newItems,
+        currentItem:{
+          text:'',
+          key: ''
+        }
+      })
+    }
+  }
+
   render() {
     return (
     <div className="App">
       <header>
         <h1>TODO App</h1>
       </header>
-      <form id="todo-form">
-        <input type="text" placeholder="Text.." />
+      <form id="todo-form" onSubmit={this.addItem}>
+        <input type="text" placeholder="Text.." value={this.state.currentItem.text} onChange={this.handleInput}/>
       </form>
     </div>
     );
